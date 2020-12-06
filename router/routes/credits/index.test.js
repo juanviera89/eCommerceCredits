c293dc3834094bb695f008c542e6a6c8bfeb33d6
Juan Viera <jviera@marsol.cl>
2020-12-05 23:10:41 -0300

    credits delete tests

--- router/routes/credits/index.test.js
+++ router/routes/credits/index.test.js
@@ -21,7 +21,7 @@ describe('/credits', function () {
             basic = `Basic ${Buffer.from('test3@user.cl' + ':' + 'password').toString('base64')}`;
             client = await models.client.create({ email: 'test3@client.cl', name: 'Test Client 3' }, { returning: ['id','email'] });
             store = await models.store.create({ name: 'Store3', clientId: client.id }, { returning: ['id', 'name'] });
-            credit = await models.credit.create({ storeId: store.id, credits: Math.ceil(Math.random() * 9999999) }, { returning: ['id', 'credits'] });
+            credit = await models.credit.create({ storeId: store.id, credits: 10 + Math.ceil(Math.random() * 9999999) }, { returning: ['id', 'credits'] });
             done()
         })
     })
@@ -113,7 +113,7 @@ describe('/credits', function () {
                     done();
                 });
         })
-        it('Should return a json object with transactions list and status 200 limited by user', function (done) {
+        it('Should return a json object with store`s credits and status 200 ', function (done) {
             chai.request(app.getApp('main'))
                 .post(`/credits?client=${client.email}&store=${store.name}&amount=5`).set('Authorization', bearer)
                 .end((err, res) => {
@@ -127,6 +127,71 @@ describe('/credits', function () {
                 });
         })
     })
+    
+    describe('DELETE', function () {
+        before(function (done) {
+            chai.request(app.getApp('main'))
+            .get('/auth').set('Authorization', basic)
+                .end((err, res) => {
+                    const response = err ? err.response : res;
+                    const body = response.body
+                    bearer = `Bearer ${body.authToken}`;
+                    done();
+                });
+
+        })
+        it('Should return a json object with error message and status 400 on client param malformation', function (done) {
+            chai.request(app.getApp('main'))
+                .delete(`/credits?client=NoEmail`).set('Authorization', bearer)
+                .end((err, res) => {
+                    const response = err ? err.response : res;
+                    response.should.have.status(400);
+                    response.body.should.be.a('object');
+                    const body = response.body
+                    body.message.should.be.a('string')
+                    done();
+                });
+        })
+        it('Should return a json object with error message on no client/store combination found and status 404', function (done) {
+            chai.request(app.getApp('main'))
+                .delete(`/credits?client=noclient@email.cl&store=SomeStore&amount=7`).set('Authorization', bearer)
+                .end((err, res) => {
+                    const response = err ? err.response : res;
+                    response.should.have.status(404);
+                    response.body.should.be.a('object');
+                    const body = response.body
+                    body.message.should.be.a('string')
+
+                    done();
+                });
+        })
+        it('Should return a json object with store`s credits and status 200 ', function (done) {
+            chai.request(app.getApp('main'))
+                .delete(`/credits?client=${client.email}&store=${store.name}&amount=7`).set('Authorization', bearer)
+                .end((err, res) => {
+                    const response = err ? err.response : res;
+                    response.should.have.status(200);
+                    response.body.should.be.a('object');
+                    const body = response.body
+                    body.credits.should.be.a('number')
+                    body.credits.should.lt(Number(credit.credits))
+                    done();
+                });
+        })
+        it('Should return a json object with insuficient funds message and status 409', function (done) {
+            chai.request(app.getApp('main'))
+                .delete(`/credits?client=${client.email}&store=${store.name}&amount=${Number(credit.credits) + 100}`).set('Authorization', bearer)
+                .end((err, res) => {
+                    const response = err ? err.response : res;
+                    response.should.have.status(409);
+                    response.body.should.be.a('object');
+                    const body = response.body
+                    body.message.should.be.a('string')
+                    done();
+                });
+        })
+    })
+    
     after(async function () {
         await models.access_token.destroy({ where: { userId: user.id } })
         await models.transaction.destroy({ where: { userId: user.id } })
